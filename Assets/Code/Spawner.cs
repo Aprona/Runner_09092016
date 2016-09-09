7fc83bc6168b11365f4c0a218b2a368385640ee6
Teemu Arponen <teemu.arponen@cs.tamk.fi>
2016-09-09 11:25:07 +0300

    Initial commit

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Runner
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _spawnPrefabs;
+        [SerializeField]
+        private float _spawntimeMin;
+        [SerializeField]
+        private float _spawnTimeMax;
+
+        private void Awake()
+        {
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            var prefab = _spawnPrefabs[Random.Range(0, _spawnPrefabs.Length)];
+            var instance = Instantiate(prefab, transform.position, Quaternion.identity);
+            Invoke("Spawn", Random.Range(_spawntimeMin, _spawnTimeMax));
+        }
+  
+    }
+}
